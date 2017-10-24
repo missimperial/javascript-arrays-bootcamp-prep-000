2f5dc2fd50cae38bce0a7439a8467c577a31b596
Kat <kathybuggs@gmail.com>
2017-10-24 01:53:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,6 +10,6 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  const array = [element, ...array]
+  var array = [element, ...array]
   return array
 }
